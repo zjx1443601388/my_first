e5389075072e512f86b5afb8e364442e650b1118
zjx1443601388 <1443601388@qq.com>
2017-12-30 18:00:49 +0800

    change Bs4_test1.py

--- Bs4_test1.py
+++ Bs4_test1.py
@@ -14,17 +14,17 @@ soup = BeautifulSoup(r.text,'html.parser',from_encoding='utf-8')
 content=[]
 
 for mulu in soup.find_all(class_="mulu"):
-    h2 = mulu.find('h2')
-        if h2 != None:
-	        h2_title = h2.string
-		        #print h2_title
-			        list1=[]
-				        for a in mulu.find(class_='box').find_all('a'):
-					            href = a.get('href')
-						                #print href
-								            box_title = a.get('title')
-									                #print box_title
-											            list1.append({'href':href,'box_title':box_title})
-												            content.append({'title':h2_title,'content':list1})
-													    with open('F:/\Git/\qiye.json','wb') as fp:
-													        json.dump(content,fp=fp,indent=4)
+	h2 = mulu.find('h2')
+	if h2 != None:
+		h2_title = h2.string
+		#print h2_title
+		list1=[]
+		for a in mulu.find(class_='box').find_all('a'):
+			href = a.get('href')
+			#print href
+			box_title = a.get('title')
+			#print box_title
+			list1.append({'href':href,'box_title':box_title})
+		content.append({'title':h2_title,'content':list1})
+with open('F:/\Git/\qiye.json','wb') as fp:
+	json.dump(content,fp=fp,indent=4)
